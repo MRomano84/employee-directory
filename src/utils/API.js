9bd1aca8d39b8f86b14565e5432490f501674c0f
Matthew Romano <mathiasrok@gmail.com>
2021-05-02 11:55:02 -0400

    fixed to export object

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 
-// export default{
+const getUsers = () => {
+    return axios.get("https://randomuser.me/api/?results=50");
+};
+
+//Must export as object
+const exportedObj = {
+    getUsers
+};
+
+export default exportedObj;
+
+
+// export default {
 //     getUsers: function() {
 //         return axios.get("https://randomuser.me/api/?results=100&nat=us");
 //     },
 // };
 
-
-export const getUsers = function() {
-    return axios.get("https://randomuser.me/api/?results=100nat=us");
-};
+// export default getUsers;
